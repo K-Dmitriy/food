90de8b3294d9c91764dc86aff68858b097e3c6bd
d.kozlov <d.kozlov@hottelecom.by>
2020-09-04 15:32:33 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -34,4 +34,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
     hideTabs();
     showTabs();
+
+    //timer
+    const finishDay = '2020-09-11';
+    
+    function getRemainingTime(endtime) {
+
+        const total = Date.parse(endtime) - Date.now();
+        let days = Math.floor(total / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((total / (1000 * 60)) % 60),
+            seconds = Math.floor((total / 1000) % 60);
+        
+        return { total, days, hours, minutes, seconds };
+    }
+
+    function setRemainingTime(selector, endtime) {
+        const timer = document.querySelector(selector),
+              fieldDay = timer.querySelector('#days'),
+              fieldHour = timer.querySelector('#hours'),
+              fieldMinute = timer.querySelector('#minutes'),
+              fieldSecond = timer.querySelector('#seconds'),
+              intervalUpdateTimeID = setInterval(updateTime, 1000);
+
+        updateTime();
+
+        function updateTime() {
+            const { total, days, hours, minutes, seconds } = getRemainingTime(endtime);
+
+            fieldDay.textContent = days;
+            fieldHour.textContent = addZero(hours);
+            fieldMinute.textContent = addZero(minutes);
+            fieldSecond.textContent = addZero(seconds);
+
+            if (total <= 0) clearInterval(intervalUpdateTimeID);
+        }
+    }
+
+    function addZero(number) {
+        return number < 10 ? '0' + number : number;
+    }
+
+    setRemainingTime('.timer', finishDay);
 });
