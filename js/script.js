f5533ef497c640a0a025d1e4abe0f4dcd28b826e
d.kozlov <d.kozlov@hottelecom.by>
2020-09-21 14:52:39 +0300

    add slider dots

--- js/script.js
+++ js/script.js
@@ -185,7 +185,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
 
-    //отправка формы
+    //send form
     const forms = document.querySelectorAll('form');
     const responseStatus = {
         load: 'img/form/spinner.svg',
@@ -257,7 +257,7 @@ document.addEventListener('DOMContentLoaded', () => {
             prevModalDialog.classList.add('show');
             prevModalDialog.classList.remove('hide');
             closeModal();
-        }, 4000);
+        }, 40000);
     }
 
     //slider
@@ -268,7 +268,10 @@ document.addEventListener('DOMContentLoaded', () => {
           offerSliderBtnPrev = offerSlider.querySelector('.offer__slider-prev'),
           offerSliderBtnNext = offerSlider.querySelector('.offer__slider-next'),
           offerSlidesWrap = offerSlider.querySelector('.offer__slider-wrapper'),
-          slideWidth = parseInt(window.getComputedStyle(offerSlidesWrap).width);
+          slideWidth = parseInt(window.getComputedStyle(offerSlidesWrap).width),
+          sliderField = document.createElement('div'),
+          indicators = document.createElement('ol'),
+          dot = document.createElement('li');
     let slideIndex = 1,
         sliderOffset = 0;
 
@@ -298,21 +301,70 @@ document.addEventListener('DOMContentLoaded', () => {
     //     changeSlide(-1);
     // });
 
-    //carousel
-    let sliderField = document.createElement('div');
-
-    sliderField.classList.add('offer__slider-inner');
-    sliderField.style.display = 'flex';
-    sliderField.style.width = 100 * offerSlides.length + '%';
-    sliderField.style.transition = '0.5s all';
+    //slider dots
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    dot.classList.add('dot');
+    dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+    `;
+    
+    offerSlidesWrap.style.position = 'relative';
+    offerSlidesWrap.append(indicators);
 
     offerSlides.forEach(slide => {
         slide.style.width = slideWidth + 'px';
         sliderField.append(slide);
+        indicators.append(dot.cloneNode());
     });
 
     offerSlidesWrap.style.overflow = 'hidden';
     offerSlidesWrap.append(sliderField);
+
+    const dots = offerSlidesWrap.querySelectorAll('.dot');
+    dots[0].style.opacity = 1;
+
+    function changeActiveDot(index) {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[index].style.opacity = 1;
+    }
+
+    dots.forEach((dot, i) => {
+        dot.addEventListener('click', () => {
+            changeActiveDot(i);
+            sliderField.style.transform = `translateX(-${sliderOffset = slideWidth * i}px)`;
+            numCurrentSlide.textContent = addZero(slideIndex = i + 1);
+        });
+    });
+
+    //carousel
+    sliderField.classList.add('offer__slider-inner');
+    sliderField.style.display = 'flex';
+    sliderField.style.width = 100 * offerSlides.length + '%';
+    sliderField.style.transition = '0.5s all';
 
     offerSliderBtnNext.addEventListener('click', () => {
         numCurrentSlide.textContent = addZero(slideIndex += 1);
@@ -325,6 +377,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         sliderField.style.transform = `translateX(-${sliderOffset}px)`;
+        changeActiveDot(slideIndex - 1);
     });
 
     offerSliderBtnPrev.addEventListener('click', () => {
@@ -338,6 +391,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         sliderField.style.transform = `translateX(-${sliderOffset}px)`;
+        changeActiveDot(slideIndex - 1);
     });
 
     numTotalSlide.textContent = addZero(offerSlides.length);
